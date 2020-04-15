afc34e2ebd9bc3f343ae220d90df6ad806981590
gay0ung <58800923+gay0ung@users.noreply.github.com>
2020-04-15 22:18:08 +0900

    del.re

--- 과제/4.vueRouter/src/views/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="text-xs-center">
-    This is About Page
-    <h1>click</h1>
-  <router-link :to="{name: 'home'}"> 
-    <h1>click</h1>
-  </router-link>
-  </div>
-</template>
--- 과제/6.vuex Router/src/views/Home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <v-container>
-    <h1>Home</h1>
-    <h4>모든 유저수: {{ $store.getters.allUsersCount }}</h4>
-  </v-container>
-</template>
