bf845a2535b334659e6ff428d4c0bdd34309127d
gay0ung <58800923+gay0ung@users.noreply.github.com>
2020-05-08 23:54:34 +0900

    todolist_book

---
+++ todolist-vue(doitBOOK)/vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+h1 {
+  color: rgb(21, 63, 119);
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
