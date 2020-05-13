334f9296bf7a60480f172cd3006085c77fa1fdb8
gay0ung <58800923+gay0ung@users.noreply.github.com>
2020-05-13 15:54:12 +0900

    사용자 입력폼 만들기

--- 인프런_vue.js(me)/vue-cli/src/App.vue
+++ 인프런_vue.js(me)/vue-cli/src/App.vue
@@ -1,29 +1,29 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-on:renew="renewStr" v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
-  // 인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: "App",
+  data() {
+    return {
+      str: "this is app header"
+    };
+  },
   components: {
-    HelloWorld
+    "app-header": AppHeader
+  },
+  methods: {
+    renewStr() {
+      this.str = "send it!";
+    }
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
