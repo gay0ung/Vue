7ec80692562b3974782eaded3398e59002f0ac19
gay0ung <58800923+gay0ung@users.noreply.github.com>
2020-05-20 16:07:45 +0900

    delete

--- 인프런_vue.js(me)/vue-cli/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import App from "./App.vue";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  // template를 실행하면 render함수가 실행한다.
-  render: (h) => h(App),
-}).$mount("#app");
